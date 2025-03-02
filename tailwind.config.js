b536ab4a7d77e963d9dae4a3ea53bffe64eda936
Datacodellc <datacode1982@gmail.com>
2025-03-03 01:16:42 +0800

    Анхны коммит
    анх оруулсан файл

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        main:['Samsung Sharp Sans'],
+      },
+      colors: {
+      primary: "#00b8b2",
+      secondary: "#444",
+      dark: "#333"
+      
+    },
+  },
+  },
+  variants: {
+    display: ["responsive", "group-hover"]
+  },
+  plugins: [],
+}
+
+                                                
